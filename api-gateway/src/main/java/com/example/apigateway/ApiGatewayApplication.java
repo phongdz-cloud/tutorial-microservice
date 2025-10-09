857fc2c06f62183d0958ad620f2e1fe015fcec80
phonghh <longtam20015@gmail.com>
2025-10-10 00:36:39 +0700

    Monitoring and configuration k8s

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,11 +1,14 @@
 package com.example.apigateway;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class ApiGatewayApplication {
     public static void main(String[] args) {
+        log.info("Starting ApiGatewayApplication v2");
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 }
--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -3,11 +3,13 @@ package com.example.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class UserServiceApplication {
     public static void main(String[] args) {
+        log.info("Starting UserServiceApplication v3");
         SpringApplication.run(UserServiceApplication.class, args);
     }
 }
-
-
